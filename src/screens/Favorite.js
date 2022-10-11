ce05fdb29e4b7ca4d41b630aee904b9d99c8d524
Josue Ramirez <josueramirez0110@gmail.com>
2022-10-11 13:29:06 -0500

    Add React Navigation configuration

---
+++ src/screens/Favorite.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Favorite = () => {
+  return (
+    <SafeAreaView>
+      <Text>Estamos en la pantalla de Pokémons Favoritos</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Favorite;
---
+++ src/screens/Pokedex.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Pokedex = () => {
+  return (
+    <SafeAreaView>
+      <Text>Estamos en la pantalla de Pokédex</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Pokedex;
